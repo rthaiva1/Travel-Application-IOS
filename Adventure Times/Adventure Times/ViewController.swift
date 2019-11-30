4b7dd9e52df093a427aeed1de85a45cacd7ff94d
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MBP.lan1>
2019-11-29 21:23:34 -0500

    implemented multiple view controllers and seague

--- Adventure Times/Adventure Times/ViewController.swift
+++ Adventure Times/Adventure Times/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+         performSegue(withIdentifier: "viewtable", sender: self)
+    }
 }
 
