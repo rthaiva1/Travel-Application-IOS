7ca6b2d70ad56ed2eec5efe017949fa9a81e44fb
System Administrator <root@Roshs-MBP.lan1>
2019-12-01 22:12:43 -0500

    Cell layout

--- Adventure Times/Adventure Times/TableViewCell.swift
+++ Adventure Times/Adventure Times/TableViewCell.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var image: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        Init()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        Init()
         // Configure the view for the selected state
+    }
+    
+    func Init()
+    {
+
     }
     
 }
