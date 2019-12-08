b3bcd561886e62cde76c2f962e885ba1e8165937
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MBP.lan1>
2019-12-07 22:55:21 -0500

    Go back function working

--- Adventure Times/Adventure Times/Description.swift
+++ Adventure Times/Adventure Times/Description.swift
@@ -11,35 +11,15 @@ import UIKit
 class Description: UIViewController {
 
     var y: String!
+    @IBOutlet var uimage: UIImageView!
     @IBOutlet weak var labeld: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         labeld.text = y
-//        winimage.image = UIImage(named: "champions")
-//        country.text = c
-//        year.text = y
-//        switch c {
-//        case "Australia":
-//            flag.image = UIImage(named: "1")
-//        case "India":
-//            flag.image = UIImage(named: "2")
-//        case "West Indies":
-//            flag.image = UIImage(named: "3")
-//        case "England":
-//            flag.image = UIImage(named: "4")
-//        case "Srilanka":
-//            flag.image = UIImage(named: "5")
-//        case "Pakistan":
-//            flag.image = UIImage(named: "6")
-//        case "New Zealand":
-//            flag.image = UIImage(named: "7")
-//        default:
-//            print("Image not found")
-//        }
     }
 
-    @IBAction func goback(_ sender: Any)
+    @IBAction func onclick(_ sender: Any)
     {
-//         performSegue(withIdentifier: "home2", sender: self)
+                 performSegue(withIdentifier: "getback", sender: self)
     }
 }
