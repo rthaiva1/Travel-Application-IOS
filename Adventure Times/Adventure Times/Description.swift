1defbacb1af82bd685ec773bc7ff48436918d1f8
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MBP.lan1>
2019-12-07 23:32:53 -0500

    Updating description view controller

--- Adventure Times/Adventure Times/Description.swift
+++ Adventure Times/Adventure Times/Description.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class Description: UIViewController {
 
+    let desc =
+        ["Great Pyramid of Giza",
+         "Great Wall of China",
+         "Petra - Jordan",
+         "Christ the Redeemer - Brazil",
+         "Machu Picchu - Peru",
+         "Chichen Itza - Mexico",
+         "Colosseum - Italy",
+         "Taj Mahal - India"]
     var y: String!
     @IBOutlet var uimage: UIImageView!
     @IBOutlet weak var labeld: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        labeld.text = y
+        uimage.image = UIImage(named: y)
+        labeld.text = desc[Int(y)! + 1]
     }
 
     @IBAction func onclick(_ sender: Any)
