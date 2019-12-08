d772dd22d36425264c6d4afe0f98fbb633cbeffc
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MBP.lan1>
2019-12-08 01:47:29 -0500

    Adding collection view

--- Adventure Times/Adventure Times/Description.swift
+++ Adventure Times/Adventure Times/Description.swift
@@ -8,28 +8,45 @@
 
 import UIKit
 
-class Description: UIViewController {
-
+class Description: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    @IBOutlet weak var viewCollection: UICollectionView!
+    
     let desc =
-        ["Great Pyramid of Giza",
-         "Great Wall of China",
-         "Petra - Jordan",
-         "Christ the Redeemer - Brazil",
-         "Machu Picchu - Peru",
-         "Chichen Itza - Mexico",
-         "Colosseum - Italy",
-         "Taj Mahal - India"]
+        ["The Great Pyramid of Giza is the oldest and largest of the three pyramids in the Giza pyramid complex bordering present-day Giza in Greater Cairo, Egypt. It is the oldest of the Seven Wonders of the Ancient World, and the only one to remain largely intact.",
+         "The Great Wall of China is the collective name of a series of fortification systems generally built across the historical northern borders of China to protect and consolidate territories of Chinese states and empires against various nomadic groups of the steppe and their polities.",
+         "Petra is a famous archaeological site in Jordan's southwestern desert. Dating to around 300 B.C., it was the capital of the Nabatean Kingdom. Accessed via a narrow canyon called Al Siq, it contains tombs and temples carved into pink sandstone cliffs, earning its nickname, the Rose City. ",
+         "Christ the Redeemer is an Art Deco statue of Jesus Christ in Rio de Janeiro, Brazil, created by French sculptor Paul Landowski and built by Brazilian engineer Heitor da Silva Costa, in collaboration with French engineer Albert Caquot. Romanian sculptor Gheorghe Leonida fashioned the face.",
+         "Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments and panoramic views. Its exact former use remains a mystery.",
+         "Chichén Itzá is a complex of Mayan ruins on Mexico's Yucatán Peninsula. A massive step pyramid, known as El Castillo or Temple of Kukulcan, dominates the ancient city, which thrived from around 600 A.D. to the 1200s. Graphic stone carvings survive at structures like the ball court, Temple of the Warriors and the Wall of the Skulls. Nightly sound-and-light shows illuminate the buildings' sophisticated geometry.",
+         "The Colosseum or Coliseum, also known as the Flavian Amphitheatre, is an oval amphitheatre in the centre of the city of Rome, Italy. Built of travertine limestone, tuff, and brick-faced concrete, it was the largest amphitheatre ever built at the time and held 50,000 spectators.",
+         "The Taj Mahal is an ivory-white marble Islamic mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself."]
     var y: String!
     @IBOutlet var uimage: UIImageView!
     @IBOutlet weak var labeld: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         uimage.image = UIImage(named: y)
-        labeld.text = desc[Int(y)! + 1]
+        labeld.text = desc[Int(y)! - 1]
+        
+        self.viewCollection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectcell")
+        viewCollection.delegate = self
+        viewCollection.dataSource = self
     }
 
-    @IBAction func onclick(_ sender: Any)
+    @IBAction func onClick(_ sender: Any)
     {
-                 performSegue(withIdentifier: "getback", sender: self)
+        performSegue(withIdentifier: "getback", sender: self)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectcell", for: indexPath)
+        cell.backgroundView = UIImageView.init(image: UIImage.init(named: y))
+        // Configure the cell...
+        return cell
+    }
+    
 }
